3e7b67aed50cd6d67373733b7a4b7a67d740081d
kgryte <kgryte@gmail.com>
2015-11-30 16:38:08 -0800

    Add grid, move prez initialization, and update content

--- TODO.md
+++ TODO.md
@@ -5,4 +5,34 @@ TODO
 2. create server (?)
 	-	is this necessary?
 	-	examples?
-3. 
+3. presentation template
+	- 	generator (`@kgryte/generator-reveal-presentation`)
+4. contributor photos
+	-	circles
+	- 	from github
+	-	link to github profiles
+	-	twitter if applicable
+5. ga
+6. instrument to track presentation metrics
+7. instead of bullets for contact, replace with relevant icons (e.g., octicon, twitter bird, mail logo)
+	-	`list-style: none;`
+	-	add font-awesome icon in `li` and add `margin-right: 10px;`
+	- 	may be more clever ways
+	-	list or horizontal columns?
+8. add link styling (hover, coloring, etc)
+9. for `wishlist`, remove bullets; add images
+	-	simd
+	-	operator overloading
+10. switch to using [grid-layouts](https://hacks.mozilla.org/2015/09/the-future-of-layout-with-css-grid-layouts/)
+	-	[polyfill](https://github.com/FremyCompany/css-grid-polyfill)
+11. sign-up slide
+	-	provide email input box
+	-	register to receive updates and news regarding the project
+12. 
+
+
+
+## Other
+
+1. move gists into separate repos
+2. 
--- public/js/lib/reveal/init.js
+++ public/js/lib/reveal/init.js
@@ -23,30 +23,120 @@
 
 	var deps = [
 		{
+			// Classlist polyfill:
 			'src': 'js/lib/reveal/classList.js',
 			'condition': checkClassList
 		},
 		{
+			// Code syntax highlighting:
 			'src': 'js/lib/reveal/plugin/highlight/highlight.js',
 			'async': true,
 			'callback': initHighlightJS
 		},
 		{
+			// Zoom plugin:
 			'src': 'js/lib/reveal/plugin/zoom-js/zoom.js',
 			'async': true
 		},
 		{
+			// Speaker notes:
 			'src': 'js/lib/reveal/plugin/notes/notes.js',
 			'async': true
 		}
 	];
 
 	Reveal.initialize({
+		// Presentation size (scaled based on resolution and maintains aspect ratio; can be percentage units):
+		'width': 960,
+		'height': 700,
+
+		// Factor of display size that should remain empty:
+		'margin': 0.1,
+
+		// Minimum and maximum scale values to apply to content when resizing:
+		'minScale': 0.2,
+		'maxScale': 1.5,
+
+		// Show the controls in the bottom right corner?
 		'controls': false,
+
+		// Show a presentation progress bar?
 		'progress': false,
+
+		// Show the page number of the current slide?
+		'slideNumber': false,
+
+		// Store slide progression in the browser history?
 		'history': true,
-		'center': true,
+
+		// Enable keyboard shortcuts for navigation?
+		'keyboard': true,
+
+		// Enable the slide overview mode?
+		'overview': true,
+
+		// Vertically center slides? If `false`, can center manually by applying a "center" class to individual slides.
+		'center': false,
+
+		// Enable touch navigation on touch devices?
+		'touch': true,
+
+		// Loop the presentation?
+		'loop': false,
+
+		// Right-to-left presentation direction?
+		'rtl': false,
+
+		// Globally turn on fragments?
+		'fragments': true,
+
+		// Is the presentation running in an embedded mode (i.e., limited screen portion)?
+		'embedded': false,
+
+		// Should a help overlay be shown when the questionmark key is pressed?
+		'help': true,
+
+		// Should speaker notes be visible to all viewers?
+		'showNotes': false,
+
+		// Number of milliseconds before proceeding to next slide (disabled when set to 0 and can be overwritten using a `data-autoslide` attribute on a slide):
+		'autoSlide': 0,
+
+		// Should auto-sliding be stopped after user input?
+		'autoSlideStoppable': true,
+
+		// Enable slide navigation via mouse wheel?
+		'mouseWheel': false,
+
+		// Hide address bar on mobile devices?
+		'hideAddressBar': true,
+
+		// Open links in an iframe preview overlay?
+		'previewLinks': false,
+
+		// Slide transition style (none, fade, slide, convex, concave, zoom):
 		'transition': 'slide',
+
+		// Slide transition speed (default, fast, slow):
+		'transitionSpeed': 'default',
+
+		// Background transition style (none, fade, slide, convex, concave, zoom):
+		'backgroundTransition': 'default',
+
+		// Number of neighboring slides in each direction relative to current slide to show in overview:
+		'viewDistance': 3,
+
+		// Parallax background image (url):
+		'parallaxBackgroundImage': '',
+
+		// Parallax background image size (css syntax: "2100px 900px"):
+		'parallaxBackgroundSize': '',
+
+		// Number of pixels to move the parallax background per slide (calculated automatically unless specified; set to 0 to disable along an axis):
+		'parallaxBackgroundHorizontal': null,
+		'parallaxBackgroundVertical': null,
+
+		// Presentation dependencies:
 		'dependencies': deps
 	});
 })();
